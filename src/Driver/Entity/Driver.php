82c26238d1fe5e789795aa67112db99b0fd2791b
Solène <tennis00015@gmail.com>
2025-10-01 21:08:54 +0200

    add class Driver

---
+++ src/Driver/Entity/Driver.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Driver\Entity;
+
+use App\User\Entity\User;
+
+final class Driver extends User
+{
+    public function __construct(
+        //Champs pour Driver
+        private ?bool $food,
+        private ?bool $music,
+        private ?bool $pets,
+        private ?bool $smoker,
+        private ?bool $speaker,
+        // Champs pour User
+        string $id,
+        string $pseudo,
+        string $mail,
+        string $password,
+        ?int $credit,
+        ?string $photo,
+        int $idRole,
+        bool $isActivated,
+    ) {
+        parent::__construct($id, $pseudo, $mail, $password, $credit, $photo, $idRole, $isActivated);
+    }
+    
+    public function getFood(): ?bool
+    {
+        return $this->food;
+    }
+
+    public function getMusic(): ?bool
+    {
+        return $this->music;
+    }
+    public function getPets(): ?bool
+    {
+        return $this->pets;
+    }
+    public function getSmoker(): ?bool
+    {
+        return $this->smoker;
+    }
+    public function getSpeaker(): ?bool
+    {
+        return $this->speaker;
+    }
+}
