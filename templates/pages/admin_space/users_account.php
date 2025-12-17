d79fdf5603a625c16db185628b3f9c44ae294dae
Solène <tennis00015@gmail.com>
2025-12-17 16:12:22 +0100

    feat(adminSpace) : Addition of users account management, display, activation, and deactivation

--- templates/pages/admin_space/users_account.php
+++ templates/pages/admin_space/users_account.php
@@ -6,3 +6,85 @@
     <button class="btn main-tab-btn" style="width:100%;"
         onclick="window.location.href='<?= BASE_URL ?>/espace-admin/statistiques'">Statistiques</button>
 </nav>
+
+<section>
+    <div class="main-header m-tb-20">
+        <h2 class="text-green text-bold">Gérer les comptes des utilisateurs
+            (<?= count($passengersList) + count($driversList) + count($passengersAndDriversList) ?>)
+        </h2>
+    </div>
+
+    <h3 style="color: black;">Passagers</h3>
+    <div class="half-separation m-tb-20 gap-12">
+        <?php
+        foreach ($passengersList as $user): ?>
+            <div class="block-light-grey">
+                <div class="flex-row flex-between">
+                    <div class="flex-column">
+                        <span class="text-bold">
+                            <?= htmlspecialchars($user['pseudo']) ?>
+                        </span>
+                        <span class="italic">
+                            <?= htmlspecialchars($user['mail']) ?>
+                        </span>
+                    </div>
+                    <?php if ($user['is_activated'] === 1): ?>
+                        <button class="btn bg-light-red suspend-user" data-id="<?= (string) $user['id'] ?>">Suspendre</button>
+                    <?php elseif ($user['is_activated'] === 0): ?>
+                        <button class="btn bg-light-green reactivate-user" data-id="<?= (string) $user['id'] ?>">Réactiver</button>
+                    <?php endif; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+    <h3 style="color: black;">Chauffeurs</h3>
+    <div class="half-separation m-tb-20 gap-12">
+        <?php
+        foreach ($driversList as $user): ?>
+            <div class="block-light-grey">
+                <div class="flex-row flex-between">
+                    <div class="flex-column">
+                        <span class="text-bold">
+                            <?= htmlspecialchars($user['pseudo']) ?>
+                        </span>
+                        <span class="italic">
+                            <?= htmlspecialchars($user['mail']) ?>
+                        </span>
+                    </div>
+                    <?php if ($user['is_activated'] === 1): ?>
+                        <button class="btn bg-light-red suspend-user" data-id="<?= (string) $user['id'] ?>">Suspendre</button>
+                    <?php elseif ($user['is_activated'] === 0): ?>
+                        <button class="btn bg-light-green reactivate-user" data-id="<?= (string) $user['id'] ?>">Réactiver</button>
+                    <?php endif; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+    <h3 style="color: black;">Passagers-Chauffeurs</h3>
+    <div class="half-separation m-tb-20 gap-12">
+        <?php
+        foreach ($passengersAndDriversList as $user): ?>
+            <div class="block-light-grey">
+                <div class="flex-row flex-between">
+                    <div class="flex-column">
+                        <span class="text-bold">
+                            <?= htmlspecialchars($user['pseudo']) ?>
+                        </span>
+                        <span class="italic">
+                            <?= htmlspecialchars($user['mail']) ?>
+                        </span>
+                    </div>
+                    <?php if ($user['is_activated'] === 1): ?>
+                        <button class="btn bg-light-red suspend-user" data-id="<?= (string) $user['id'] ?>">Suspendre</button>
+                    <?php elseif ($user['is_activated'] === 0): ?>
+                        <button class="btn bg-light-green reactivate-user" data-id="<?= (string) $user['id'] ?>">Réactiver</button>
+                    <?php endif; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
+
+<script src="<?= ASSETS_PATH ?>js/admin_space.js" defer></script>
